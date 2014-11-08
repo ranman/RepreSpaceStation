2379cf796b9113c251e7296503eed80065189afd
Joseph Spens <joseph@workmarket.com>
2014-11-08 00:40:41 -0500

    Add html templates

--- application.py
+++ application.py
@@ -1,5 +1,15 @@
+from flask import Flask, render_template
 from eve import Eve
+from os.path import abspath, dirname
+
 app = Eve()
+app.root_path = abspath(dirname(__file__))
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
+	app.debug = True
 	app.run()
